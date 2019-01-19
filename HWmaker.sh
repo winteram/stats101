181e58af530a031ad19b6663dae7d12d34e2a663
Winter Mason <winteram@gmail.com>
2019-01-19 18:13:18 -0500

    Moved HWmaker to root folder. Modified Week1 to be more generic.

--- HWmaker.sh
+++ HWmaker.sh
@@ -3,20 +3,22 @@
 if [ $# -lt 2 ];
     then
     scriptname=`basename $0`
-    echo "usage: $scriptname <filename> <N>"
-    echo "<filename> is prefix to *.Rnw"
-    echo "<N> is number of homeworks to generate"
+    echo "usage: $scriptname <weeknumber> <N>"
+    echo "<weeknumber> is the week number (in the folder name)"
+    echo "<N> is number of homeworks / exams to generate"
     echo ""
     echo "author: winter mason <m@winteram.com>"
     exit 1
 else
-    fname=$1
+    dno=$1
     nusers=$2
 fi
 
 for (( c=1; c<=$nusers; c++ ))
 do
     string=`echo ""$RANDOM" "16" o p " | dc`
+    cd Week$dno
+    fname=`ls *.Rnw | cut -d'.' -f1`
     cp $fname.Rnw $fname\_$string.Rnw
     sed -i -e 's/UNIQUEKEY/'$string'/g' $fname\_$string.Rnw
     R CMD Sweave $fname\_$string.Rnw
@@ -26,7 +28,10 @@ do
     sed -i -e 's/noprintanswers/printanswers/g' $fname\_$string\_key.tex
     pdflatex -interaction nonstopmode $fname\_$string\_key.tex
     pdflatex -interaction nonstopmode $fname\_$string\_key.tex
+    [ -d HWs ] || mkdir HWs
+    [ -d Keys ] || mkdir Keys
     mv $fname\_$string.pdf HWs
     mv $fname\_$string\_key.pdf Keys
     rm $fname\_$string*.*
+    cd ..
 done
--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This contains files to teach an introductory-level statistics course.  This was originally designed for business students, so may cover different material than other statistics courses.
 
 Each week has slides for the lecture and R code to generate homeworks.
+
+To generate homeworks / exams, run:
+
+./HWmaker.sh WeekX N
+
+Where WeekX is the week for which to create homeworks, and N is the number of homeworks to make.
+
+Requirements: 
+- bash
+- R
+- pdflatex
+- Sweave
