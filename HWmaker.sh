2a27a3bea07281ddf5513faba197985e23a90bb1
Winter Mason <winteram@gmail.com>
2019-01-19 19:13:24 -0500

    Documented HWmaker and updated HWs 1-3

--- HWmaker.sh
+++ HWmaker.sh
@@ -14,20 +14,42 @@ else
     nusers=$2
 fi
 
+# For each student
 for (( c=1; c<=$nusers; c++ ))
 do
+    # Create a random string for the homework
     string=`echo ""$RANDOM" "16" o p " | dc`
+
+    # move to the folder
     cd Week$dno
+
+    # Get the file name
     fname=`ls *.Rnw | cut -d'.' -f1`
+
+    # Make a copy for the student
     cp $fname.Rnw $fname\_$string.Rnw
+
+    # Replace the UNIQUEKEY with the random string
     sed -i -e 's/UNIQUEKEY/'$string'/g' $fname\_$string.Rnw
+
+    # Run the R code to generate the tex files
     R CMD Sweave $fname\_$string.Rnw
+
+    # Make the PDF
     pdflatex -interaction nonstopmode $fname\_$string.tex
     pdflatex -interaction nonstopmode $fname\_$string.tex
+
+    # Make a copy for the anser key
     cp $fname\_$string.tex $fname\_$string\_key.tex
+
+    # Modify script to show answers
     sed -i -e 's/noprintanswers/printanswers/g' $fname\_$string\_key.tex
+
+    # Make the PDF
     pdflatex -interaction nonstopmode $fname\_$string\_key.tex
     pdflatex -interaction nonstopmode $fname\_$string\_key.tex
+    
+    # Clean up the file folder
     [ -d HWs ] || mkdir HWs
     [ -d Keys ] || mkdir Keys
     mv $fname\_$string.pdf HWs
--- README.md
+++ README.md
@@ -6,12 +6,16 @@ Each week has slides for the lecture and R code to generate homeworks.
 
 To generate homeworks / exams, run:
 
-./HWmaker.sh WeekX N
+./HWmaker.sh **X** **N**
 
-Where WeekX is the week for which to create homeworks, and N is the number of homeworks to make.
+Where Week**X** is the week for which to create homeworks, and N is the number of homeworks to make.
 
 Requirements: 
 - bash
 - R
 - pdflatex
 - Sweave
+
+## Warning!
+
+I have not checked every piece of content in every slide to ensure it is copyright-safe.  If you find something that violates copyright or fair use, please let me know and I will modify accordingly.
